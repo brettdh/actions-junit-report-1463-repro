0851691601a70e8718a0748cd0d5398d325bf48e
Brett Higgins <brethigg@cisco.com>
2025-12-15 18:19:16 -0500

    Establish passing tests

---
+++ test_b.py
@@ -0,0 +1,5 @@
+def test_a():
+    assert True
+
+def test_b():
+    assert True
---
+++ test_classes.py
@@ -0,0 +1,14 @@
+class TestClassA:
+    def test_a(self):
+        assert True
+
+    def test_b(self):
+        assert True
+
+
+class TestClassB:
+    def test_a(self):
+        assert True
+
+    def test_b(self):
+        assert True
